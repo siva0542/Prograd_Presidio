49de97d22573ba17478bbca7f2edc5fc122db556
Siva Bhargava Adi <sivabhargavakanduru@gmail.com>
2021-08-17 17:36:23 +0530

    If Length and Breadth values are Positive

--- src/main/java/RectangleTestDemo.java
+++ src/main/java/RectangleTestDemo.java
@@ -1,2 +1,8 @@
 public class RectangleTestDemo {
+    public int area(int length, int breadth) {
+        if(length>0 && breadth>0)
+        return length*breadth;
+        else
+            return 0;
+    }
 }
--- src/test/java/RectanglePropertiesTest.java
+++ src/test/java/RectanglePropertiesTest.java
@@ -1,2 +1,22 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class RectanglePropertiesTest {
+    RectangleTestDemo rectangle=new RectangleTestDemo();
+
+    @Test
+    public void toGetAreaTwentyWhenLengthIsFiveAndBreadthIsFour(){
+        int actualArea=rectangle.area(5,4);
+        int expectedArea=20;
+        assertEquals(actualArea,expectedArea);
+    }
+
+    @Test
+    public void toGetAreaSixteenWhenLengthAndBreadthAreEqualToFour(){
+        int actualArea=rectangle.area(4,4);
+        int expectedArea=16;
+        assertEquals(actualArea,expectedArea);
+    }
+
 }
